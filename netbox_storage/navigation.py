16c36603fd655c3dd776a02246985e3696d7adf9
Gabor SOMOGYVARI <gaborsomogyvari@gmail.com>
2023-02-24 12:51:38 +0100

    permission fixes in navigation and templates

--- netbox_storage/navigation.py
+++ netbox_storage/navigation.py
@@ -1,124 +1,115 @@
 from extras.plugins import PluginMenuButton, PluginMenuItem, PluginMenu
 from utilities.choices import ButtonColorChoices
 
-storagepool_buttons = [
-    PluginMenuButton(
-        link='plugins:netbox_storage:storagepool_add',
-        title='Add',
-        icon_class='mdi mdi-plus-thick',
-        color=ButtonColorChoices.GREEN
-    ),
-    PluginMenuButton(
-        link='plugins:netbox_storage:storagepool_import',
-        title='Import',
-        icon_class='mdi mdi-upload',
-        color=ButtonColorChoices.CYAN
-    )
-]
 
-lun_buttons = [
-    PluginMenuButton(
-        link='plugins:netbox_storage:lun_add',
-        title='Add',
-        icon_class='mdi mdi-plus-thick',
-        color=ButtonColorChoices.GREEN
-    ),
-    PluginMenuButton(
-        link='plugins:netbox_storage:lun_import',
-        title='Import',
-        icon_class='mdi mdi-upload',
-        color=ButtonColorChoices.CYAN
-    )
-]
-
-datastore_buttons = [
-    PluginMenuButton(
-        link='plugins:netbox_storage:datastore_add',
-        title='Add',
-        icon_class='mdi mdi-plus-thick',
-        color=ButtonColorChoices.GREEN
-    ),
-    PluginMenuButton(
-        link='plugins:netbox_storage:datastore_import',
-        title='Import',
-        icon_class='mdi mdi-upload',
-        color=ButtonColorChoices.CYAN
-    )
-]
-
-storagesession_buttons = [
-    PluginMenuButton(
-        link='plugins:netbox_storage:storagesession_add',
-        title='Add',
-        icon_class='mdi mdi-plus-thick',
-        color=ButtonColorChoices.GREEN
-    ),
-    PluginMenuButton(
-        link='plugins:netbox_storage:storagesession_import',
-        title='Import',
-        icon_class='mdi mdi-upload',
-        color=ButtonColorChoices.CYAN
-    )
-]
-
-vmdk_button = [
-    PluginMenuButton(
-        link='plugins:netbox_storage:vmdk_add',
-        title='Add',
-        icon_class='mdi mdi-plus-thick',
-        color=ButtonColorChoices.GREEN
-    ),
-    PluginMenuButton(
-        link='plugins:netbox_storage:vmdk_import',
-        title='Import',
-        icon_class='mdi mdi-upload',
-        color=ButtonColorChoices.CYAN
-    )
-]
-
-storagepool_item = (
-    PluginMenuItem(
-        link='plugins:netbox_storage:storagepool_list',
-        link_text='Storage Pools',
-        permissions=['netbox_storage.view_storagepool'],
-        buttons=storagepool_buttons
-    )
+storagepool_item = PluginMenuItem(
+    link='plugins:netbox_storage:storagepool_list',
+    link_text='Storage Pools',
+    permissions=['netbox_storage.view_storagepool'],
+    buttons=[
+        PluginMenuButton(
+            link='plugins:netbox_storage:storagepool_add',
+            title='Add',
+            icon_class='mdi mdi-plus-thick',
+            color=ButtonColorChoices.GREEN,
+            permissions=['netbox_storage.add_storagepool'],
+        ),
+        PluginMenuButton(
+            link='plugins:netbox_storage:storagepool_import',
+            title='Import',
+            icon_class='mdi mdi-upload',
+            color=ButtonColorChoices.CYAN,
+            permissions=['netbox_storage.add_storagepool'],
+        )
+    ]
 )
 
-lun_item = (
-    PluginMenuItem(
-        link='plugins:netbox_storage:lun_list',
-        link_text='LUNs',
-        buttons=lun_buttons,
-        permissions=['netbox_storage.view_lun'],
-    )
+lun_item = PluginMenuItem(
+    link='plugins:netbox_storage:lun_list',
+    link_text='LUNs',
+    permissions=['netbox_storage.view_lun'],
+    buttons=[
+        PluginMenuButton(
+            link='plugins:netbox_storage:lun_add',
+            title='Add',
+            icon_class='mdi mdi-plus-thick',
+            color=ButtonColorChoices.GREEN,
+            permissions=['netbox_storage.add_lun'],
+        ),
+        PluginMenuButton(
+            link='plugins:netbox_storage:lun_import',
+            title='Import',
+            icon_class='mdi mdi-upload',
+            color=ButtonColorChoices.CYAN,
+            permissions=['netbox_storage.add_lun'],
+        )
+    ]
 )
 
-datastore_item = (
-    PluginMenuItem(
-        link='plugins:netbox_storage:datastore_list',
-        link_text='Datastores',
-        buttons=datastore_buttons,
-        permissions=['netbox_storage.view_datastore'],
-    )
+datastore_item = PluginMenuItem(
+    link='plugins:netbox_storage:datastore_list',
+    link_text='Datastores',
+    permissions=['netbox_storage.view_datastore'],
+    buttons=[
+        PluginMenuButton(
+            link='plugins:netbox_storage:datastore_add',
+            title='Add',
+            icon_class='mdi mdi-plus-thick',
+            color=ButtonColorChoices.GREEN,
+            permissions=['netbox_storage.add_datastore'],
+        ),
+        PluginMenuButton(
+            link='plugins:netbox_storage:datastore_import',
+            title='Import',
+            icon_class='mdi mdi-upload',
+            color=ButtonColorChoices.CYAN,
+            permissions=['netbox_storage.add_datastore'],
+        )
+    ]
 )
 
-storagesession_item = (
-    PluginMenuItem(
-        link='plugins:netbox_storage:storagesession_list',
-        link_text='Storage Sessions',
-        buttons=storagesession_buttons,
-        permissions=['netbox_storage.view_storagesession'],
-    )
+storagesession_item = PluginMenuItem(
+    link='plugins:netbox_storage:storagesession_list',
+    link_text='Storage Sessions',
+    permissions=['netbox_storage.view_storagesession'],
+    buttons=[
+        PluginMenuButton(
+            link='plugins:netbox_storage:storagesession_add',
+            title='Add',
+            icon_class='mdi mdi-plus-thick',
+            color=ButtonColorChoices.GREEN,
+            permissions=['netbox_storage.add_storagesession'],
+        ),
+        PluginMenuButton(
+            link='plugins:netbox_storage:storagesession_import',
+            title='Import',
+            icon_class='mdi mdi-upload',
+            color=ButtonColorChoices.CYAN,
+            permissions=['netbox_storage.add_storagesession'],
+        )
+    ]
 )
 
-vmdk_item = (
-    PluginMenuItem(
-        link='plugins:netbox_storage:vmdk_list',
-        link_text='VMDKs',
-        buttons=vmdk_button,
-        permissions=['netbox_storage.view_vmdk'],
-    )
+vmdk_item = PluginMenuItem(
+    link='plugins:netbox_storage:vmdk_list',
+    link_text='VMDKs',
+    permissions=['netbox_storage.view_vmdk'],
+    buttons=[
+        PluginMenuButton(
+            link='plugins:netbox_storage:vmdk_add',
+            title='Add',
+            icon_class='mdi mdi-plus-thick',
+            color=ButtonColorChoices.GREEN,
+            permissions=['netbox_storage.add_vmdk'],
+        ),
+        PluginMenuButton(
+            link='plugins:netbox_storage:vmdk_import',
+            title='Import',
+            icon_class='mdi mdi-upload',
+            color=ButtonColorChoices.CYAN,
+            permissions=['netbox_storage.add_vmdk'],
+        )
+    ]
 )
 
 menu = PluginMenu(
